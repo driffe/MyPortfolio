0f56b570488b0852ba37b7124d35dd3b94bfb9b0
SeyoungOh <33598679+driffe@users.noreply.github.com>
2024-01-10 14:09:32 +0000

    change some style

--- js/script.js
+++ js/script.js
@@ -73,3 +73,11 @@ function addListenersToLinks() {
 
 document.body.onload = addListenersToLinks();  
 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
